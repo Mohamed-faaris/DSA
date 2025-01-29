5d3a4c49dcba3c0b99171c244ac9cfecdeabcdbd
Mohamed Faaris <74762980+Mohamed-faaris@users.noreply.github.com>
2025-01-29 23:57:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Implement Pow/implement-pow.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    double power(double b, int e) {
+        // code here
+        // double res = b;
+        // int s = e<0 ? -1:1;
+        // long ex = e*s;
+        // while(e){
+        //     if(e%2) res = res*res;
+        //     e = e<<1;
+        // }
+        return pow(b,e);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    cout << fixed << setprecision(5);
+    int t;
+    cin >> t;
+    while (t--) {
+        double b;
+        cin >> b;
+        int e;
+        cin >> e;
+        Solution ob;
+        cout << ob.power(b, e) << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
