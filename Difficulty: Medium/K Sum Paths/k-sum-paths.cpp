0d49710db01b7b1d88c844257d5668c296f61f2e
Mohamed Faaris <74762980+Mohamed-faaris@users.noreply.github.com>
2025-02-23 13:07:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sum Paths/k-sum-paths.cpp
+++ Difficulty: Medium/K Sum Paths/k-sum-paths.cpp
@@ -95,7 +95,6 @@ struct Node {
 class Solution {
   public:
     int sumK(Node *root, int k) {
-        // code here
         unordered_map<int,int> ump;
         ump[0]=1;
         solve(root, k, ump, 0);
@@ -103,11 +102,10 @@ class Solution {
     }
     private:
         int total = 0;
-        const int mod=1e9+7;
         void solve(Node* node, int k, unordered_map<int,int> &ump, int sum){
             if(node==nullptr) return;
             sum+=node->data;
-            if(ump.find(sum-k)!=ump.end()) total=(total+ump[sum-k])%mod;
+            if(ump.find(sum-k)!=ump.end()) total=(total+ump[sum-k]);
             ump[sum]++;
             solve(node->left, k, ump, sum);
             solve(node->right, k, ump, sum);
